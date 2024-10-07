b88bcad3b8f4696f8270aefbf29023971cabfd26
Andy Madrigal Garro <64030322+AndyMadrigalG@users.noreply.github.com>
2024-10-06 23:58:38 -0600

    only numbers (#2)
    * first attempt
    
    * manifest is a json
    
    * changes for first run
    
    * logo and more country codes
    
    * changes
    
    * Better UI
    
    * ONLY ACCEPT NUMBERS

--- popup.js
+++ popup.js
@@ -1,7 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const phoneNumberInput = document.getElementById('phoneNumber');
+
+  phoneNumberInput.addEventListener('input', function() {
+    // Check if the value is not a number
+    if ( this.value.includes('.') || isNaN(this.value) ) {
+      // Remove any non-numeric characters
+      this.value = this.value.replace(/\D/g, '');
+    }
+  });
+});
+
 document.getElementById('submitBtn').addEventListener('click', function() {
     const countryCode = document.getElementById('countryCode').value;
     const phoneNumber = document.getElementById('phoneNumber').value;
     const fullNumber = countryCode + phoneNumber;
-    const newUrl = `https://wa.me/${encodeURIComponent(fullNumber)}`;
+    const newUrl = `https://web.whatsapp.com/send/?phone=${encodeURIComponent(fullNumber)}`;
     chrome.tabs.create({ url: newUrl });
-  });
+});
