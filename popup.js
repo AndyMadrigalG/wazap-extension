e6352a06ac0afdd4015318208e740e7ac2447876
Andy Madrigal Garro <64030322+AndyMadrigalG@users.noreply.github.com>
2024-10-07 00:14:16 -0600

    efficient changes (#3)

--- popup.js
+++ popup.js
@@ -1,20 +1,20 @@
-
 document.addEventListener('DOMContentLoaded', function() {
   const phoneNumberInput = document.getElementById('phoneNumber');
 
   phoneNumberInput.addEventListener('input', function() {
-    // Check if the value is not a number
-    if ( this.value.includes('.') || isNaN(this.value) ) {
-      // Remove any non-numeric characters
-      this.value = this.value.replace(/\D/g, '');
-    }
+    // Remove any non-numeric characters
+    this.value = this.value.replace(/\D/g, '');
   });
 });
 
 document.getElementById('submitBtn').addEventListener('click', function() {
-    const countryCode = document.getElementById('countryCode').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    const fullNumber = countryCode + phoneNumber;
+  const countryCode = document.getElementById('countryCode').value.trim();
+  const phoneNumber = document.getElementById('phoneNumber').value.trim();
+  if (countryCode && phoneNumber) {
+    const fullNumber = `${countryCode}${phoneNumber}`;
     const newUrl = `https://web.whatsapp.com/send/?phone=${encodeURIComponent(fullNumber)}`;
     chrome.tabs.create({ url: newUrl });
+  } else {
+    alert('Please enter both country code and phone number.');
+  }
 });
